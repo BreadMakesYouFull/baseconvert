26c9a2c07c2ffcde7d078fb812419ca6d388900b
squdle <1jd@live.co.uk>
2016-05-08 23:05:21 +0100

    Command line arguments corrected
    The command line interface now matches the module interface.

--- baseconvert/__main__.py
+++ baseconvert/__main__.py
@@ -11,10 +11,10 @@ def main():
 
     Examples:
 
-        $ python -m baseconvert -n 0.5 -i 10 -o 20
+        $ python -m baseconvert -n 0.5 -i 10 -o 20 -s True
         0.A
 
-        $ echo 3.1415926 | python -m baseconvert -i 10 -o 16 -d 3
+        $ echo 3.1415926 | python -m baseconvert -i 10 -o 16 -d 3 -s True
         3.243
     """
     # Parse arguments
@@ -27,6 +27,10 @@ def main():
                         help="The output base (default 10).")
     parser.add_argument("-d", "--max_depth", default=10, type=int,
                         help="The maximum fractional digits (default 10).")
+    parser.add_argument("-r", "--recurring", default=True, type=bool,
+                        help="Boolean, if True will attempt to find recurring decimals (default True).")
+    parser.add_argument("-s", "--string", type=bool,
+                        help="Boolean, if True will output number as String, else as tuple (default False).")
     args = parser.parse_args()
 
     args.input_base = float(args.input_base)
@@ -36,9 +40,9 @@ def main():
     if args.output_base == int(args.output_base):
         args.output_base = int(args.output_base)
     if (args.number):
-        return base(args.number, args.input_base, args.output_base, string=True, max_depth=args.max_depth)
+        return base(args.number, args.input_base, args.output_base, string=args.string, max_depth=args.max_depth, recurring=args.recurring)
     elif not sys.stdin.isatty():
-        return base(sys.stdin.read().strip(), args.input_base, args.output_base, string=True, max_depth=args.max_depth)
+        return base(sys.stdin.read().strip(), args.input_base, args.output_base, string=args.string, max_depth=args.max_depth, recurring=args.recurring)
     else:
         raise ValueError("Please input a number!")
 
